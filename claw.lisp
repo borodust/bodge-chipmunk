1e353902f4785595ff752365d4a73305a9c03509
Pavel Korolev <dev@borodust.org>
2018-01-17 00:04:43 +0300

    Update claw descriptor

--- claw.lisp
+++ claw.lisp
@@ -1,6 +1,6 @@
 (claw:c-include "bodge_chipmunk.h" bodge-chipmunk
-  :package :%cp
+  :in-package :%cp
   :include-sources ("bodge_chipmunk.h")
   :exclude-sources ("chipmunk_private\\.h$")
   :include-definitions ("(cp|CP_)\\w*")
-  :symbol-prefix ("cp" "CP_"))
+  :rename-symbols (claw:by-removing-prefixes "cp" "CP_"))
