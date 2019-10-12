0d4584f8e2eefde35c5043a19ff8511ca27d4105
Pavel Korolev <dev@borodust.org>
2019-10-12 14:32:25 +0300

    Use single-floats

--- src/bodge_chipmunk.h
+++ src/bodge_chipmunk.h
@@ -1,6 +1,8 @@
 #ifndef _BODGE_CHIPMUNK_H_
 #define _BODGE_CHIPMUNK_H_
 
+#define CP_USE_DOUBLES 0
+#define CP_USE_CGTYPES 0
 #include <chipmunk/chipmunk.h>
 
 #endif /* _BODGE_CHIPMUNK_H_ */
--- src/chipmunk.lisp
+++ src/chipmunk.lisp
@@ -3,11 +3,11 @@
 (declaim (inline v))
 (defun v (x y)
   "Allocates foreign memory for chipmunk vector. Manual memory freeing required."
-  (declare (type double-float x y))
+  (declare (type single-float x y))
   (c-let ((vec %cp:vect :alloc t))
     (setf (vec :x) x
           (vec :y) y)
     (vec &)))
 
 
-(defvar +vzero+ (v 0d0 0d0))
+(defvar +vzero+ (v 0f0 0f0))
